eb56dd37584f6710788329cb2e3ba08ccde6188a
Shaheel abbasi <shaheelabbasi456@gmail.com>
2025-06-26 15:48:52 +0500

    fixed restriction issue

--- src/Middlewares/IsRestricted.middleware.js
+++ src/Middlewares/IsRestricted.middleware.js
@@ -1,11 +1,23 @@
+const { User } = require("../Models/User.model.js");
 const Apierror = require("../Utils/ApiError.js");
 const {asyncHandler}=require("../Utils/asyncHandler.js");
+
 const IsRestricted=asyncHandler(async(req,res,next)=>{
 
-  if(req.user?.IsRestricted){
-    throw new Apierror(400,"your account has been restricted")
-  }
-    next();
+  const { email, password ,role} = req.body;
+
+  const existingUser = await User.findOne({
+    email,
+    role
+  });
+
+  if(!existingUser.IsRestricted) {
+    next()
+}
+else{
+    throw new Apierror(400,"canot login your account has been restricted")
+}
+
 })
 
 module.exports=IsRestricted
